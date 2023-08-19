b04b5a91d38601b5801621e0dbcddd9824f26459
Nicole Silvestrini <nicole.silvestrini@ws-solution.com>
2023-08-19 19:55:41 -0300

    Finalização do login de acesso

--- dw_barbershop/lib/src/core/providers/application_provider.dart
+++ dw_barbershop/lib/src/core/providers/application_provider.dart
@@ -1,9 +1,13 @@
 import 'package:dw_barbershop/src/core/restCliente/rest_client.dart';
+import 'package:dw_barbershop/src/repositories/user/user_repository.dart';
+import 'package:dw_barbershop/src/repositories/user/user_repository_impl.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'application_provider.g.dart';
 
-@Riverpod(
-  keepAlive: true,
-)
-RestClient restClient(RestClientRef) => RestClient();
+@Riverpod(keepAlive: true)
+RestClient restClient(RestClientRef ref) => RestClient();
+
+@Riverpod(keepAlive: true)
+UserRepository userRepository(UserRepositoryRef ref) =>
+    UserRepositoryIpml(restClient: ref.read(restClientProvider));
--- dw_barbershop/lib/src/core/providers/application_provider.g.dart
+++ dw_barbershop/lib/src/core/providers/application_provider.g.dart
@@ -6,7 +6,7 @@ part of 'application_provider.dart';
 // RiverpodGenerator
 // **************************************************************************
 
-String _$restClientHash() => r'753d2696c2a2c691a5501d9f994add94683390a8';
+String _$restClientHash() => r'0ee58f1fd102b2016ed621885f1e8d52ed00da66';
 
 /// See also [restClient].
 @ProviderFor(restClient)
@@ -20,5 +20,20 @@ final restClientProvider = Provider<RestClient>.internal(
 );
 
 typedef RestClientRef = ProviderRef<RestClient>;
+String _$userRepositoryHash() => r'74f4f6461caf434c9ec9cd79a02ae0ffab6bf150';
+
+/// See also [userRepository].
+@ProviderFor(userRepository)
+final userRepositoryProvider = Provider<UserRepository>.internal(
+  userRepository,
+  name: r'userRepositoryProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$userRepositoryHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef UserRepositoryRef = ProviderRef<UserRepository>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member
